20126519419685734f32fce7f5981ce7f91d23d0
Fawas Oyerinde <fawasoyeyemi@gmail.com>
2026-06-17 22:55:17 +0100

    Fixed merge conflict

--- empty_file.py
+++ empty_file.py
@@ -1,4 +1,4 @@
 print("improving git understanding through reps")
 #this was edited directly through git, will it work??? guess i'll find out in about 30 seconds.
 #it didnt work hmph 🥲
-#this was edited directly through git, will it work??? guess i'll find out in about 30 seconds.
+
