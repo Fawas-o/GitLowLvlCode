3e29536b6048f4e169ba86200c9c38cad30ed91b
Fawas-o <138376777+Fawas-o@users.noreply.github.com>
2026-06-17 23:00:39 +0100

    Add comments and print statement to empty_file.py

--- empty_file.py
+++ empty_file.py
@@ -2,3 +2,4 @@ print("improving git understanding through reps")
 #this was edited directly through git, will it work??? guess i'll find out in about 30 seconds.
 #it didnt work hmph 🥲
 
+#lowkey starting to understand git, its nothing too complicated🎉🎉
